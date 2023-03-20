8f1554aa8cfdd19d5f6da523393fa1ce2cc2fc72
Renato <Renato@DESKTOP-AGPJAMV.(none)>
2023-03-20 14:39:00 -0300

    exercicio 05

---
+++ exercicio-05/scripts.js
@@ -0,0 +1,8 @@
+for (i=1; i<=10; i++){
+    console.log(`Tabuada de ${i}`);
+    for (j=1; j<=10; j++){
+        const resultado = i * j;
+        console.log (`${i} x ${j} = ${resultado}`)
+    }
+    
+}
